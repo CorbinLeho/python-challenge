5136de6f0c15b250f33ad56c816318db9188a479
Corbin Leho <corbinleho@yahoo.com>
2018-10-30 18:11:01 -0400

    i think it may work

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,31 +6,32 @@ month = []
 profit = []
 
 # Set path for file
-bank_csv = os.path.join(../../../Desktop/Resources/PyBank.csv)
+Bank_csv = os.path.join("PyBank.csv")
 
 
 # Open the CSV
-with open(bank_csv, newline = "") as csv_file:
+with open(Bank_csv, newline = "") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-    header = next(csv_reader)
-    TotalMonths = len(csv_reader)
 
+    header = next(csv_reader)
+
+    TotalMonths = len(list(csv_reader))
+    TotalProfit = 0
     for months in csv_reader:
-      for month in months:
-          total = 0
-          Total_Profit = sum(month.row(2))
+        TotalProfit += int(row[1])
+
+
     
 
 # Set variable for output file
-output_file = os.path.join("PyBank.csv")
+output_file = os.path.join("PyBankSolved.csv")
 
 #  Open the output file
 with open(output_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
 
     # Write the header row
-    writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
-                     "Percent of Reviews", "Length of Course"])
+    writer.writerow(["Date", "Profit/Losses"])
 
     # Write in zipped rows
     writer.writerows(cleaned_csv)
