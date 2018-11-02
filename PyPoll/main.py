967362ae2a7ad647a48d7949857eb45cabb93ebb
Corbin Leho <corbinleho@yahoo.com>
2018-11-01 22:35:17 -0400

    ???

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+def Results():
+    print("Election Results")
+    print("---------------------------")
+    print("Total Votes: " + Total))
+    print("---------------------------")
+    print()
+    print("---------------------------")
+    print("Winner: " + Winner)
+    print("---------------------------")
+
+
+        
