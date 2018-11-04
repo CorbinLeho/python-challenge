22ab6578cb09008bb2a0711b1341b32ca33b6164
Corbin Leho <corbinleho@yahoo.com>
2018-11-04 11:49:34 -0500

    asdf

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,37 +17,36 @@ with open(Bank_csv, newline = "") as csv_file:
 
     TotalMonths = 0
     TotalProfit = 0
+    MonthlyChange = []
     GreatestIncrease = 0
     GreatestDecrease = 0
     for row in csv_reader:
         TotalMonths += 1
         TotalProfit += float(row[1])
-        MonthlyChange = float(row + 1 [1]) - float(row[1])
-        GreatestIncrease = max(GreatestIncrease, MonthlyChange)
-        GreatestDecrease = min(GreatestDecrease, abs(MonthlyChange))
-        
-        if abs(MonthlyChange) > abs(MonthlyChange):
-            GreatestIncrease = 
-            def AverageChange(MonthlyChange, TotalMonths):
-def Analysis():
-    print("Financial Analysis")
-    print("--------------------------")
-    print("Total Months: " + str(TotalMonths))
-    print("Total: $" + str(TotalProfit))
-    print("Average Change: $" + str(AverageChange))
-    print("Greatest Increase in Profits: " + str(GreatestIncrease))
-    print("Greatest Decrease in Profits: " + str(GreatestDecrease))
-Analysis()
+        MonthlyChange.append(float(row + 1 [1]) - float(row[1]))
+        GreatestIncrease = max(MonthlyChange)
+        GreatestDecrease = min(MonthlyChange)
+        AverageChange = sum(abs(MonthlyChange)) / len(TotalMonths)
+    
+    def Analysis():
+        print("Financial Analysis")
+        print("--------------------------")
+        print("Total Months: " + str(TotalMonths))
+        print("Total: $" + str(TotalProfit))
+        print("Average Change: $" + str(AverageChange))
+        print("Greatest Increase in Profits: " + str(GreatestIncrease))
+        print("Greatest Decrease in Profits: " + str(GreatestDecrease))
+    Analysis()
 
 # Set variable for output file
 output_file = os.path.join("PyBankSolved.csv")
 
 #  Open the output file
 with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
+    writer = txt.writer(datafile)
 
     # Write the header row
-    writer.writerow(["Date", "Profit/Losses"])
+    writer.writerow(Analysis())
 
     # Write in zipped rows
   #  writer.writerows()
