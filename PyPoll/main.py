73cc72b4f1aab8fd1add632790b19831f6de8efe
Corbin Leho <corbinleho@yahoo.com>
2018-11-06 23:50:26 -0500

    done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,66 +1,55 @@
 import os
 import csv
 
-file = os.path.join('PyPoll.csv')
+File = ('PyPoll.csv')
 
-poll = {}
+Poll = {}
 
-total_votes = 0
+TotalVotes = 0
 
-#gets data file
-with open(file, 'r') as csvfile:
+#gets data File
+with open(File, 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
 
     next(csv_reader)
 
-    #creates dictionary from file using column 3 as keys, using each name only once.
-    #counts votes for each candidate as entries
-    #keeps a total vote count by counting up 1 for each loop (# of rows w/o header)
     for row in csv_reader:
-        total_votes += 1
-        if row[2] in poll.keys():
-            poll[row[2]] = poll[row[2]] + 1
+        TotalVotes += 1
+        if row[2] in Poll.keys():
+            Poll[row[2]] = Poll[row[2]] + 1
         else:
-            poll[row[2]] = 1
- 
-#create empty list for candidates and his/her vote count
-candidates = []
-num_votes = []
+            Poll[row[2]] = 1
 
-#takes dictionary keys and values and, respectively, dumps them into the lists, 
-# candidates and num_votes
-for key, value in poll.items():
-    candidates.append(key)
-    num_votes.append(value)
+Candidates = []
+Num_votes = []
 
-# creates vote percent list
+for key, value in Poll.items():
+    Candidates.append(key)
+    Num_votes.append(value)
+
 vote_percent = []
-for n in num_votes:
-    vote_percent.append(round(n/total_votes*100, 1))
+for n in Num_votes:
+    vote_percent.append(round(n/TotalVotes*100, 1))
 
-# zips candidates, num_votes, vote_percent into tuples
-clean_data = list(zip(candidates, num_votes, vote_percent))
+# zips Candidtes, Num_votes, vote_percent into tuples
+clean_data = list(zip(Candidates, Num_votes, vote_percent))
 
-#creates winner_list to put winners (even if there is a tie)
 winner_list = []
 
 for name in clean_data:
-    if max(num_votes) == name[1]:
+    if max(Num_votes) == name[1]:
         winner_list.append(name[0])
 
-# makes winner_list a str with the first entry
 winner = winner_list[0]
 
-#only runs if there is a tie and puts additional winners into a string separated by commas
 if len(winner_list) > 1:
     for w in range(1, len(winner_list)):
         winner = winner + ", " + winner_list[w]
 
-#prints to file
 output_file = os.path.join('PyPollSolved.txt')
 
 with open(output_file, 'w') as f:
-    f.writelines('Election Results \n------------------------- \nTotal Votes: ' + str(total_votes) + 
+    f.writelines('Election Results \n------------------------- \nTotal Votes: ' + str(TotalVotes) + 
       '\n-------------------------\n')
     for entry in clean_data:
         f.writelines(entry[0] + ": " + str(entry[2]) +'%  (' + str(entry[1]) + ')\n')
