9cda1342244c8aa4c6a6ce9b25b65ade5ec3e846
Corbin Leho <corbinleho@yahoo.com>
2018-11-06 20:45:59 -0500

    a

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,23 @@
-def Results():
-    print("Election Results")
-    print("---------------------------")
-    print("Total Votes: " + Total))
-    print("---------------------------")
-    print()
-    print("---------------------------")
-    print("Winner: " + Winner)
-    print("---------------------------")
+import csv
+import os
+
+f = 'PyPoll.csv'
+
+with open (f. 'r') as csv:
+    a = 
+
+
+
+
+    def Results():
+        print("Election Results")
+        print("---------------------------")
+        print("Total Votes: " + Total))
+        print("---------------------------")
+        print()
+        print("---------------------------")
+        print("Winner: " + Winner)
+        print("---------------------------")
 
 
         
